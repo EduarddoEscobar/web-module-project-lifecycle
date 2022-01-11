f0ebc71f39875d9b433ebf0169c3569514d3cc18
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-11 17:38:12 -0500

    Got the user and put it in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import User from './components/User';
+import axios from 'axios';
+import './index.css';
 
 class App extends React.Component {
+
+  state = {
+    user: {},
+    followers: [],
+  }
+
+  componentDidMount() {
+    const handle = 'eduarddoescobar';
+    axios.get(`https://api.github.com/users/${handle}`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          ...this.state,
+          user: res.data
+        });
+      }).catch(err => console.error(err));
+  }
+
   render() {
-    return(<div>
-      Github Card
+    return(
+    <div>
+      <User user={this.state.user} />
     </div>);
   }
 }
---
+++ src/components/User.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class User extends React.Component {
+
+    state = {
+        input: ""
+    }
+
+    render(){
+        return(
+         <div>
+            <h1>Github Card</h1>
+            <form>
+                <input type="text" className="handle-input" placeholder="Github Handle"/>
+                <button>Search</button>
+            </form>
+            {this.props.user && 
+            <>
+                <div className="user-card">
+                    <img src={this.props.user.avatar_url} alt="Avatar of the user"/>
+                    <div className="user-info">
+                        <h2>{this.props.user.name}</h2>
+                        <p>({this.props.user.login})</p>
+                        <h3>TOTAL REPOS: {this.props.user.public_repos}</h3>
+                        <h3>TOTAL FOLLOWERS: {this.props.user.followers}</h3>
+                    </div>
+                </div>
+            </>
+            }
+         </div>
+        )
+    }
+}
+
+export default User;
